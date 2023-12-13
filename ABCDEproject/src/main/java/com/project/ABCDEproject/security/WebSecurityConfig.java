069647fc8ead4e1e0a623090906f6d9e05744bdb
Baek-Garim <bgr00080546@gmail.com>
2023-12-13 15:45:25 +0900

    base-2

--- ABCDEproject/src/main/java/com/project/ABCDEproject/security/WebSecurityConfig.java
+++ ABCDEproject/src/main/java/com/project/ABCDEproject/security/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig {
         .and()
         .formLogin()						//일반적인 폼을 이용한 로그인 처리/실패 방법을 사용
         .loginPage("/member/loginForm")		//시큐리티에서 제공하는 기본 폼이 아닌 사용자가 만든 폼 사용
-        .loginProcessingUrl("/member/login").permitAll()	//인증 처리를 하는 URL을 설정. 로그인 폼의 action으로 지정
+        .loginProcessingUrl("/member/login").defaultSuccessUrl("/", true).permitAll()	//인증 처리를 하는 URL을 설정. 로그인 폼의 action으로 지정
         .usernameParameter("memberid")		//로그인폼의 아이디 입력란의 name
         .passwordParameter("memberpw")		//로그인폼의 비밀번호 입력란의 name
         .and()
@@ -60,12 +60,12 @@ public class WebSecurityConfig {
         // 인증 (로그인)
         .usersByUsernameQuery(
         		"select memberid username, memberpw password, enabled " +
-                "from spring5_member " +
+                "from member " +
                 "where memberid = ?")
         // 권한
         .authoritiesByUsernameQuery(
         		"select memberid username, rolename role_name " +
-                "from spring5_member " +
+                "from member " +
                 "where memberid = ?");
     }
 
